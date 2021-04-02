0c4c093563a7943b817db3508c2a536dffa773af
Zaino <hello@darrin.dev>
2021-04-02 16:18:37 -0700

    upate script.js / entries saved to local storage

--- script.js
+++ script.js
@@ -1,17 +1,87 @@
-//* Place the the current day and time in the jumbrotron
-$("#currentDT").text(moment().format("llll"));
-
-
 //* Create global variables 
+var date = moment().format("llll");
 
 
-//* Create an event listener for the click on the Save icon
+//* Lets test moment.js 
+console.log(date);// we should see the current day, date and time
+
+//* Place the the current day and time in the jumbrotron
+//$("#currentDT").text(moment().format("llll"));
+$("#currentDT").append(date);
 
 
-//* Save inputs to local storage 
+//* Create logic to dynamically style the inputs based on time past/present/future 
 
 
-//* Clear inputs after midnight 
+//* Create an event listener for the click on the Save icon for each row 
+// 9am
+$("#save-9")//button id
+.on("click", function() {
+    var entertext = $("#9").val();
+    localStorage.setItem("9:00", entertext);
+});
+$("#9").val(localStorage.getItem("9:00"));
 
+// 10am
+$("#save-10")//button id
+.on("click", function() {
+    var entertext = $("#10").val();
+    localStorage.setItem("10:00", entertext);
+});
+$("#10").val(localStorage.getItem("10:00"));
 
-//* Create logic to dynamically style the inputs based on time past/preset/future 
+// 11am
+$("#save-11")//button id
+.on("click", function() {
+    var entertext = $("#11").val();
+    localStorage.setItem("11:00", entertext);
+});
+$("#11").val(localStorage.getItem("11:00"));
+
+// 12pm
+$("#save-12")//button id
+.on("click", function() {
+    var entertext = $("#12").val();
+    localStorage.setItem("12:00", entertext);
+});
+$("#12").val(localStorage.getItem("12:00"));
+
+// 1pm
+$("#save-13")//button id
+.on("click", function() {
+    var entertext = $("#13").val();
+    localStorage.setItem("13:00", entertext);
+});
+$("#13").val(localStorage.getItem("13:00"));
+
+// 2pm
+$("#save-14")//button id
+.on("click", function() {
+    var entertext = $("#14").val();
+    localStorage.setItem("14:00", entertext);
+});
+$("#14").val(localStorage.getItem("14:00"));
+
+// 3pm
+$("#save-15")//button id
+.on("click", function() {
+    var entertext = $("#15").val();
+    localStorage.setItem("15:00", entertext);
+});
+$("#15").val(localStorage.getItem("15:00"));
+
+// 4pm
+$("#save-16")//button id
+.on("click", function() {
+    var entertext = $("#16").val();
+    localStorage.setItem("16:00", entertext);
+});
+$("#16").val(localStorage.getItem("16:00"));
+
+// 5pm
+$("#save-17")//button id
+.on("click", function() {
+    var entertext = $("#17").val();
+    localStorage.setItem("17:00", entertext);
+});
+$("#17").val(localStorage.getItem("17:00"));
