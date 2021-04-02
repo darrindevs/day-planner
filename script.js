e1d54cb6e8ef54c2ee3e7a48a673f93f0cd66d3f
Zaino <hello@darrin.dev>
2021-04-01 17:11:08 -0700

    add script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+//* Place the the current day and time in the jumbrotron
+$("#currentDT").text(moment().format("llll"));
+
+
+//* Create global variables 
+
+
+//* Create an event listener for the click on the Save icon
+
+
+//* Save inputs to local storage 
+
+
+//* Clear inputs after midnight 
+
+
+//* Create logic to dynamically style the inputs based on time past/preset/future 
