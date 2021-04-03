d7d4c3930b80e2e7c1c6a18557353cf965fd5efa
Zaino <hello@darrin.dev>
2021-04-02 18:45:10 -0700

    add updated event listener in script.js

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 //* Create global variables 
 var date = moment().format("llll");
 
-
 //* Lets test moment.js 
-console.log(date);// we should see the current day, date and time
+console.log("it is now " + date);// we should see the current day, date and time
 
 //* Place the the current day and time in the jumbrotron
 //$("#currentDT").text(moment().format("llll"));
@@ -12,76 +11,190 @@ $("#currentDT").append(date);
 
 //* Create logic to dynamically style the inputs based on time past/present/future 
 
+// Moment.js gets current date and time 
+var now = new Date().getHours();
+console.log(now);
+
+
+if (now > 9) {
+    $("#9").addClass("past");
+} else if (now >= 9 && now < 10) {
+    $("#9").addClass("present");
+} else if (now < 9) {
+    $("#9").addClass("future");
+}
+
+if (now > 10) {
+    $("#10").addClass("past");
+} else if (now >= 10 && now < 11) {
+    $("#10").addClass("present");
+} else if (now < 10) {
+    $("#10").addClass("future");
+}
+
+if (now > 11) {
+    $("#11").addClass("past");
+} else if (now >= 11 && now < 12) {
+    $("#11").addClass("present");
+} else if (now < 11) {
+    $("#11").addClass("future");
+}
+
+if (now > 12) {
+    $("#12").addClass("past");
+} else if (now >= 12 && now < 13) {
+    $("#12").addClass("present");
+} else if (now < 12) {
+    $("#12").addClass("future");
+}
+
+if (now > 13) {
+    $("#13").addClass("past");
+} else if (now >= 13 && now < 14) {
+    $("#13").addClass("present");
+} else if (now < 13) {
+    $("#13").addClass("future");
+}
+
+if (now > 14) {
+    $("#14").addClass("past");
+} else if (now >= 14 && now < 15) {
+    $("#14").addClass("present");
+} else if (now < 14) {
+    $("#14").addClass("future");
+}
+
+if (now > 15) {
+    $("#15").addClass("past");
+} else if (now >= 15 && now < 16) {
+    $("#15").addClass("present");
+} else if (now < 15) {
+    $("#15").addClass("future");
+}
+
+if (now > 16) {
+    $("#16").addClass("past");
+} else if (now >= 16 && now < 17) {
+    $("#16").addClass("present");
+} else if (now < 16) {
+    $("#16").addClass("future");
+}
+
+if (now > 17) {
+    $("#17").addClass("past");
+} else if (now >= 17 && now < 18) {
+    $("#17").addClass("present");
+} else if (now < 17) {
+    $("#17").addClass("future");
+}
+
+if (now > 18) {
+    $("#18").addClass("past");
+} else if (now >= 18 && now < 19) {
+    $("#17").addClass("present");
+} else if (now < 18) {
+    $("#18").addClass("future");
+};
+
+//* One click listener 
+
+var rowContEl = $("#row-container");// we are listening on the entire container 
+rowContEl.on('click', '.row-btn', function (event) {
+    // within the rowContEl container we want to target clicks on the buttons = row-btn classes
+    console.log("hello");// print hello if the click event is working 
+    console.log(event.target);// now print the element that is clicked 
+    var elementID = $(this).attr('id'); // make var elementID the value of the id attribute
+    console.log("hey" + elementID);// check it by console logging - the result is "save-9" etc - the button id
+    console.log(elementID.slice(5));// check to see what we get if we slice the first 5 characters = 9
+    elementID = elementID.slice(5); // we now want to update elementID to equal "9" instead of "save-9" because....
+    var text = $("#" + elementID).val();//because we want to make var text equal to the input id of "9", so we do some slicing and concatenation to set the value of text to "#9" etc
+    localStorage.setItem(elementID + ":00", text);// we now concatenate elementID with ":00" because that is the format in the code we are keeping 
+
+//todo update the html like 9
+  });
+
 
 //* Create an event listener for the click on the Save icon for each row 
 // 9am
-$("#save-9")//button id
-.on("click", function() {
-    var entertext = $("#9").val();
-    localStorage.setItem("9:00", entertext);
-});
+//$("#save-9")//button id
+//.on("click", function() {
+   // var text = $("#9").val();
+    //localStorage.setItem("9:00", text);
+//});
 $("#9").val(localStorage.getItem("9:00"));
 
 // 10am
-$("#save-10")//button id
+$("#save-10")
 .on("click", function() {
-    var entertext = $("#10").val();
-    localStorage.setItem("10:00", entertext);
+    var text = $("#10").val();
+    localStorage.setItem("10:00", text);
 });
 $("#10").val(localStorage.getItem("10:00"));
 
 // 11am
-$("#save-11")//button id
+$("#save-11")
 .on("click", function() {
-    var entertext = $("#11").val();
-    localStorage.setItem("11:00", entertext);
+    var text = $("#11").val();
+    localStorage.setItem("11:00", text);
 });
 $("#11").val(localStorage.getItem("11:00"));
 
 // 12pm
-$("#save-12")//button id
+$("#save-12")
 .on("click", function() {
-    var entertext = $("#12").val();
-    localStorage.setItem("12:00", entertext);
+    var text = $("#12").val();
+    localStorage.setItem("12:00", text);
 });
 $("#12").val(localStorage.getItem("12:00"));
 
 // 1pm
-$("#save-13")//button id
+$("#save-13")
 .on("click", function() {
-    var entertext = $("#13").val();
-    localStorage.setItem("13:00", entertext);
+    var text = $("#13").val();
+    localStorage.setItem("13:00", text);
 });
 $("#13").val(localStorage.getItem("13:00"));
 
 // 2pm
-$("#save-14")//button id
+$("#save-14")
 .on("click", function() {
-    var entertext = $("#14").val();
-    localStorage.setItem("14:00", entertext);
+    var text = $("#14").val();
+    localStorage.setItem("14:00", text);
 });
 $("#14").val(localStorage.getItem("14:00"));
 
 // 3pm
-$("#save-15")//button id
+$("#save-15")
 .on("click", function() {
-    var entertext = $("#15").val();
-    localStorage.setItem("15:00", entertext);
+    var text = $("#15").val();
+    localStorage.setItem("15:00", text);
 });
 $("#15").val(localStorage.getItem("15:00"));
 
 // 4pm
-$("#save-16")//button id
+$("#save-16")
 .on("click", function() {
-    var entertext = $("#16").val();
-    localStorage.setItem("16:00", entertext);
+    var text = $("#16").val();
+    localStorage.setItem("16:00", text);
 });
 $("#16").val(localStorage.getItem("16:00"));
 
 // 5pm
-$("#save-17")//button id
+$("#save-17")
 .on("click", function() {
-    var entertext = $("#17").val();
-    localStorage.setItem("17:00", entertext);
+    var text = $("#17").val();
+    localStorage.setItem("17:00", text);
 });
 $("#17").val(localStorage.getItem("17:00"));
+
+//todo clear the saved at end of day
+
+$("#reset")//button id
+.on("click", function() {
+    localStorage.cache.delete();
+    console.log(click);
+});
+
+
+//todo remove 6pm
+
